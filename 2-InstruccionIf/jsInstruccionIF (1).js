7d4093faf9e69d1d632ca337a5d76c91958c444f
fedepugliese94 <fedepugliese94@gmail.com>
2016-07-06 20:12:40 -0300

    Primer commit con ejercicios hechos hasta el tercero.

---
+++ 2-InstruccionIf/jsInstruccionIF (1).js
@@ -0,0 +1,9 @@
+function Mostrar()
+{
+//tomo la edad  
+	var edad;
+	edad = document.getElementById('edad').value;
+	if (edad == 15) {alert("Niña Bonita");} //tengo que poner dos (==) porque si pongo uno solo estoy asignando 
+
+
+}//FIN DE LA FUNCIÓN
---
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -0,0 +1,12 @@
+function Mostrar()
+{
+//tomo la edad  
+var edad;
+edad = document.getElementById('edad').value;
+/*if (edad >= 18) {alert("Usted es mayor de edad");} //Para poner que es mayor o igual poner siempre primero el simbolo < o > y despues el =
+if (edad < 18) {alert("Usted es menor de edad");} 
+*/
+if (edad >= 18) {alert("Usted es mayor de edad");} 
+else {alert("Usted es menor de edad");}
+
+}//FIN DE LA FUNCIÓN
