99a98c8347ee3c1794088e147764f967f51d27b8
fedepugliese94 <fedepugliese94@gmail.com>
2016-07-06 20:20:13 -0300

    Segundo cambio con el ejercicio 4 completo de dos formas diferentes.

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -3,8 +3,10 @@ function Mostrar()
 //tomo la edad  
 	var edad;
 	edad = document.getElementById('edad').value;
-	if (edad >= 13) {alert("Usted es adolescente");}
-	if (edad ==< 17) {alert("Usted es adolescente");}
+	/*if (edad >= 13 && edad < 18) {alert("Usted es adolescente");}
+	//esta es la forma correcta de hacerlo
+	*/
+	if (edad > 12) { if (edad < 18) {alert("Usted es adolescente");}}
 
 
 }//FIN DE LA FUNCIÓN
