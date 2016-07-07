2d5a31d4ada32fc619fabad2c6e3d00a7cf2cdf3
fedepugliese94 <fedepugliese94@gmail.com>
2016-07-06 22:02:43 -0300

    Tercer commit con varias anotaciones.

--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -1,6 +1,22 @@
 function Mostrar()
 {
 //tomo la edad  
+	var edad;
+	edad = document.getElementsByTagName('edad').value;
+	/*if (edad < 13 && edad > 17) {}
+	else {alert("Usted NO es adolescente");}
+	esta es una de las formas correctas de hacerlo
+	*/
 
-
+	if (edad < 13) 
+			{
+				alert("Usted NO es adolescente");
+			}
+	else 
+		{
+			if (edad > 17) 
+				{
+					alert("Usted NO es adolescente");
+				}
+		}		
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,8 +1,54 @@
 function Mostrar()
 {
 //tomo la edad  
+	var edad;
+	edad = document.getElementById('edad').value;
+	/*if (edad > 17) 
+		{
+			alert("Usted es un ADULTO");
+		}
+	if (edad < 13) 
+		{
+			alert("Usted es un NIÑO");
+		}
+	if (edad < 17 && edad > 13) 
+		{
+			alert("Usted es un adolescente");
+		}
+	*/
 
 
+	/*if (edad < 13) 
+		{
+			alert("Usted es MENOR");
+		}
+	if (edad <17 && edad >12) 
+		{
+			alert("Usted es adolescente");
+		}
+	else
+		{
+			alert("Usted es MAYOR");
+		}
 
+	ESTO NO HAY QUE HACERLO JAMAS, ESTA MAL, MUY MAL! 	
+	*/
+
+
+	if (edad < 13) 
+		{
+			alert("Usted es MENOR");
+		}	
+	else 
+		{
+			if (edad > 18) 
+				{
+					alert("Usted es MAYOR");
+				}
+			else 
+				{
+					alert("Usted es ADOLESCENTE");
+				}	
+		}	
 
 }//FIN DE LA FUNCIÓN
