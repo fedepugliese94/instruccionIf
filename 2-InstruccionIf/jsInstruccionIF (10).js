d1b4d1a7f16fa265f833248c2ae3dd03b09d3d89
fedepugliese94 <fedepugliese94@gmail.com>
2016-07-13 19:11:28 -0300

    eJERCICIO 9 HECHO Y CON EXPLICACION

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,6 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
-
+	var numero = math.random(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+	alert(numero);
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -1,6 +1,28 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
+	var numero =(Math.random()*10)+1;
+	alert(numero);
+	//ESTE ES EL RESULTADO DEL EJERCICIO
 	
+
+	//El Math.Floor(Math.random) este es para que de solo el nuemro redondeado. 
+	
+
+	/*var numero = Math.floor(Math.random()*10);
+	alert(numero);
+	*/
+
+
+	/*var numero = Math.floor(Math.random()*5)+20;
+	alert(numero);
+	*/
+
+
+//EL Math.random es para que te de numeros con coma.
+	/*var numero =(Math.random()*10)+2;
+	alert(numero);
+	*/
+
 
 }//FIN DE LA FUNCIÓN
